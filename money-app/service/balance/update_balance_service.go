9a507ce76790d646828427069dd4c7bfcede5bb8
wjtnk <r.sugikawa@sonicmoov.com>
2021-05-31 10:49:48 +0900

    memo追加

--- money-app/service/balance/update_balance_service.go
+++ money-app/service/balance/update_balance_service.go
@@ -6,11 +6,17 @@ import (
 	"money-app/repository"
 )
 
+// Goにはクラスがないので構造体で定義
+// type HogeがそのままHogeというクラスになる
 type UpdateBalanceService struct {
+	// 使用したい外部クラスを定義
 	BalanceRepository           repository.BalanceRepository
 	IdempotentCheckerRepository repository.IdempotentCheckerRepository
 }
 
+// UpdateBalanceServiceクラスに属するクラスメソッドとして定義
+// 先頭の1文字をエイリアスとして定義するのが通常らしい
+// (UpdateBalanceService => u)
 func (u UpdateBalanceService) Exec(userId string, amount int, idempotentKey string) error {
 	db := db.GetDB()
 	return db.Transaction(func(tx *gorm.DB) error {
